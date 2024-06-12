f17c29739b0a9f0db4ffc89d08ec01d521881fa7
Monu010615 <monu010615@gmail.com>
2024-06-12 22:05:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7102700263:AAGTuauAVCgOvxhaX47K7Moq8z8_
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6959409818))
-LOG = -1002052942726
+LOG = -1002024765915
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6959409818").split()):
